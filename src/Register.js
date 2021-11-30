0e89688f7a2675319bb45b52e705216629ba7743
Kalana <it19005690@my.sliit.lk>
2021-11-30 13:19:15 +0530

    2nd todoapp attemt

---
+++ src/Register.js
@@ -0,0 +1,11 @@
+function Register() {
+    return(
+        <form action="">
+            <input type="email" placeholder="email"/> <br/>
+            <input type="password" placeholder="passwprd"/><br/>
+            <button type="submit">Register</button>
+        </form>
+    );
+}
+
+export default Register;
